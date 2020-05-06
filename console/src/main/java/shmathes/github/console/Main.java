4675b1dc584f242d9f9e8f84a6d7e9d51489a24e
Shawn Mathes <shawnmathes8807@gmail.com>
2020-05-06 11:56:00 -0400

    Created console module and moved Main class from the core module to console.

--- console/src/main/java/shmathes/github/console/Main.java
+++ console/src/main/java/shmathes/github/console/Main.java
@@ -1,10 +1,13 @@
-package shmathes.github;
+package shmathes.github.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import shmathes.github.AppConfig;
+import shmathes.github.MessageGenerator;
+import shmathes.github.NumberGenerator;
 
 public class Main
 {
